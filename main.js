572120e18b480f7705200009478b70c821af979b
vtnam2705 <thanhnamkh2705@gmail.com>
2022-05-06 11:09:08 +0700

    BC29_js-buoi7-8_VoThanhNam - Upload files

---
+++ main.js
@@ -0,0 +1,262 @@
+function getID(id) {
+    return document.getElementById(id);
+}
+
+// Nhập mảng
+var arrList = [];
+getID("btn-array").onclick = function () {
+    var number = Number(getID("txtArray").value)
+    // var emptyArr = '';
+
+    arrList.push(number);
+
+    // New array
+    getID("newArray").innerHTML = '[' + arrList.toString() + ']';
+    getID("newArray").classList.add("alert-success");
+}
+
+
+/* ====================================================================== */
+// Bài 1 - tính tổng các số dương trong mảng
+// function tinhTong(arrList) {
+//     var tong = 0;
+//     for (var i = 0; i < arrList.length; i++) {
+//         if(arrList[i] >= 0) {
+//             tong += arrList[i];
+//         }
+//     }
+//     return tong;
+// }
+
+// var arrList = [1,-5,6,7,8,-9];
+// console.log(tinhTong(arrList));
+
+/* ----------------------------------------------------- */
+getID("btn-result").onclick = function () {
+    // var arrList = getID("txtMang").value;
+    var sum = 0;
+
+    for (var i = 0; i < arrList.length; i++) {
+        if (arrList[i] >= 0) {
+            sum += arrList[i];
+        }
+    }
+
+    getID("result").innerHTML = `Tổng tất cả số dương trong mảng [${arrList}]: ${sum}`;
+
+    getID("result").classList.add("alert-success");
+}
+
+
+
+/* ====================================================================== */
+// Bài 2 - Đếm số dương trong mảng
+getID("btn-ketQua").onclick = function () {
+    var count = 0;
+
+    for (var i = 0; i < arrList.length; i++) {
+        if (arrList[i] > 0) {
+            count++;
+        }
+    }
+
+    getID("ketQua").innerHTML = `Có ${count} số dương trong mảng [${arrList}]`;
+
+    getID("ketQua").classList.add("alert-success");
+}
+
+/* ====================================================================== */
+// Bài 3 - tìm số nhỏ nhất trong mảng
+getID("btn-console").onclick = function () {
+    var min = arrList[0];
+
+    for (var i = 0; i < arrList.length; i++) {
+        if (arrList[i] < min) {
+            min = arrList[i];
+        }
+    }
+
+    getID("console").innerHTML = `Số nhỏ nhất trong mảng [${arrList}]: ${min}`;
+
+    getID("console").classList.add("alert-success");
+}
+
+/* ====================================================================== */
+// Bài 4 - Tìm số dương nhỏ nhất trong mảng
+// var array = [1,5,6,-7,8,-10,10];
+
+// function soDuong(array) {
+//     var min = -1;
+
+//     for(var i = 0; i < array.length; i++) {
+//         if((min == -1 || array[i] < min) && array[i] > 0) {
+//             min = array[i];
+//         }
+//     }
+//     return min;
+// }
+
+// console.log(soDuong(array))
+
+getID("btn-inKetQua").onclick = function () {
+    var minNum = -1;
+
+    for (var i = 0; i < arrList.length; i++) {
+        if ((minNum == -1 || arrList[i] < minNum) && arrList[i] > 0) {
+            minNum = arrList[i];
+        }
+    }
+
+    getID("inKetQua").innerHTML = `Số dương nhỏ nhất trong mảng [${arrList}]: ${minNum}`;
+
+    getID("inKetQua").classList.add("alert-success");
+}
+/* ====================================================================== */
+// Bài 5 - Tìm số chẵn cuối cùng của mảng
+getID("btn-lastNum").onclick = function () {
+    for (var i = arrList.length - 1; i >= 0; i--) {
+        if (arrList[i] % 2 == 0 && arrList[i] > 0) {
+            getID("lastNum").innerHTML = `Số chẵn cuối cùng trong mảng  [${arrList}]: ${arrList[i]}`;
+            break;
+        }
+        getID("lastNum").innerHTML = -1;
+    }
+}
+
+
+/* ====================================================================== */
+// Bài 6 - Đổi chỗ 2 giá trị trong mảng theo vị trí 
+// Lấy giá trị đầu tiên trong mảng
+function getA(arrList) {
+    for (var index1 = 0; index1 < arrList.length; index1++) {
+        return arrList[index1];
+    }
+}
+
+// Lấy giá trị thứ 2 trong mảng
+function getB(arrList) {
+    for (var index2 = 0; index2 < arrList.length; index2++) {
+        return arrList[index2];
+    }
+}
+
+// Đổi chỗ vị trí 2 phần thử trong mảng
+var flag;
+
+getID("btn-doiCho").onclick = function() {
+    var print = '';
+
+    var index1 = getID("vitri1").value;
+    console.log(index1);
+
+    var index2 = getID("vitri2").value;
+    console.log(index2);
+
+    if (index1 <= arrList.length && index2 <= arrList.length) {
+        flag = getA(arrList);
+        getA(arrList) = getB(arrList);
+        getB(arrList) = flag;
+        print = `Mảng sau khi đổi chỗ 2 vị trí ${index1} và ${index2}: [${arrList}]`;
+    } else {
+        print = `Không tìm thấy giá trị thứ ${index1} hoặc thứ ${index2} trong mảng. Vui lòng kiểm tra lại phần tử trong mảng!!!!`;
+    }
+
+    getID("doiCho").innerHTML = print;
+}
+
+/* ====================================================================== */
+// Bài 7 - Sắp xếp mảng tăng dần
+getID("btn-tangdan").onclick = function () {
+    for (var i = 0; i < arrList.length - 1; i++) {
+        for (var j = i + 1; j < arrList.length; j++) {
+            if (arrList[i] > arrList[j]) {
+                var temp;
+                temp = arrList[i];
+                arrList[i] = arrList[j];
+                arrList[j] = temp;
+            }
+        }
+        getID("xepTangDan").innerHTML = `Mảng [${arrList}] sau khi được sắp xếp tăng dần`;
+
+        getID("xepTangDan").classList.add("alert-success");
+    }
+}
+
+/* ====================================================================== */
+// Bài 8 - Tìm số nguyên tố đầu tiên
+/* Kiểm tra số nguyên tố */
+function ktSoNguyenTo(x) {
+    if (x <= 1) return 0; //0 : false
+    for (var j = 2; j <= Math.sqrt(x); j++) {
+        if (x % j == 0) return 0;
+    }
+    return 1; //1 : true
+}
+
+getID("btn-soNT").onclick = function () {
+    for (var i = 0; i <= arrList.length - 1; i++) {
+        if (ktSoNguyenTo(arrList[i])) {
+            getID("soNguyenTo").innerHTML = `Số nguyên tố đầu tiên trong mảng [${arrList}]: ${arrList[i]}`;
+            break;
+        }
+        getID("soNguyenTo").innerHTML = `-1`;
+    }
+}
+
+
+
+/* ====================================================================== */
+// Bài 9 - Đếm số nguyên
+getID("btn-demSo").onclick = function () {
+    var count = 0;
+    var ketQua = '';
+    for (var i = 0; i <= arrList.length - 1; i++) {
+        if (Number.isInteger(arrList[i])) {
+            count++;
+        }
+        ketQua = `Có tất cả ${count} số nguyên trong mảng [${arrList}]`;
+    }
+    getID("demSoNguyen").innerHTML = ketQua;
+    getID("demSoNguyen").classList.add("alert-success");
+}
+
+/* ====================================================================== */
+// Bài 10 - So sánh số lượng số âm và số dương trong mảng
+// Đếm số lượng số dương trong mảng
+function countduong(arrList) {
+    var countDuong = 0;
+
+    for (var i = 0; i <= arrList.length - 1; i++) {
+        if (arr[i] > 0) {
+            countDuong++;
+        }
+    }
+    return countDuong;
+}
+
+// Đếm số lượng số âm trong mảng
+function countam(arrList) {
+    var countAm = 0;
+
+    for (var i = 0; i <= arrList.length - 1; i++) {
+        if (arr[i] < 0) {
+            countAm++;
+        }
+    }
+    return countAm;
+}
+
+// So sáng số dương và số âm
+getID("btn-inDuongAm").onclick = function () {
+    var content = '';
+
+    if (countduong(arrList) > countam(arrList)) {
+        content = "Số dương > Số âm";
+    } else if (countduong(arrList) < countam(arrList)) {
+        content = "Số âm > Số dương";
+    } else {
+        content = "Số dương = Số âm";
+    }
+
+    getID("inDuongAm").innerHTML = content;
+}
---
+++ test.js
@@ -0,0 +1,136 @@
+var arr = [-1,9,-9,5,6,7,8,2,1,3,0,10]
+
+// function oddNum(arr) {
+//     for (var i = arr.length - 1; i >= 0; i--) {
+//         if(arr[i] > 0 && arr[i] % 2 == 0) {
+//             return arr[i];
+//             break;
+//         } 
+//     }
+//     return -1;
+// }
+
+// console.log(oddNum(arr));
+
+//Bài 6 - Đổi chỗ 2 vị trí trong mảng
+function getA(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        return arr[i];
+    }
+}
+
+function getB(arr) {
+    for (let j = 0; j < arr.length; j++) {
+        return arr[j];
+    }
+}
+
+var flag;
+
+function doiCho(i, j) {
+    if (i <= arr.length && j <= arr.length) {
+        flag = arr[i];
+        arr[i] = arr[j];
+        arr[j] = flag;
+        return arr;
+    } else {
+        return(`Không tìm thấy giá trị thứ ${i} hoặc thứ ${j} trong mảng. Vui lòng kiểm tra lại phần tử trong mảng!!!!`);
+    }
+}
+console.log(doiCho(0, 2))
+
+// // Bài 7 - Sắp xếp tăng dần
+// var hoanVi;
+// function tangDan(arr) {
+//     for (var i = 0; i < arr.length - 1; i++) {
+//         for (var j = i + 1; j < arr.length; j++) {
+//             if (arr[i] > arr[j]) {
+//                 hoanVi = arr[i];
+//                 arr[i] = arr[j];
+//                 arr[j] = hoanVi;
+//             }
+//         }
+//     }
+//     return arr;
+// }
+
+// console.log(tangDan(arr))
+
+// // Bài 8 - tìm số nguyên tố đầu tiên
+// function ktSoNguyenTo(x) {
+//     if (x <= 1) return 0; //0 : false
+//     for (var j = 2; j <= Math.sqrt(x); j++) {
+//         if (x % j == 0) return 0;
+//     }
+//     return 1; //1 : true
+// }
+
+// function soNguyenToDauTien(arr) {
+//     for(var i = 0; i <= arr.length - 1; i++) {
+//         if(ktSoNguyenTo(arr[i])) {
+//             return arr[i];
+//             break;
+//         }
+//     }
+// }
+
+// console.log(soNguyenToDauTien(arr));
+
+// // Bài 9 - Đếm số nguyên
+// function countInteger(arr) {
+    
+//     var count = 0;
+//     for (var i = 0; i <= arr.length - 1; i++) {
+//         var ketQua = '';
+        
+//         if (Number.isInteger((arr[i]))) {
+//             count++;
+//         }
+//         ketQua = `có tất cả ${count} số nguyên trong mảng [${arr}]`;
+//     }
+//     return ketQua;
+// }
+
+// console.log(countInteger(arr));
+
+// // Bài 10 - So sánh số lượng số âm và số dương trong mảng
+// // Đếm số lượng số dương trong mảng
+// function countduong(arr) {
+//     var countDuong = 0;
+
+//     for (var i = 0; i <= arr.length - 1; i++) {
+//         if(arr[i] > 0) {
+//             countDuong++;
+//         }
+//     }
+//     return countDuong;
+// }
+
+// // Đếm số lượng số âm trong mảng
+// function countam(arr) {
+//     var countAm = 0;
+
+//     for (var i = 0; i <= arr.length - 1; i++) {
+//         if(arr[i] < 0) {
+//             countAm++;
+//         }
+//     }
+//     return countAm;
+// }
+
+// // So sáng số dương và số âm
+// function soSanh() {
+//     var result = '';
+
+//     if (countduong(arr) > countam(arr)) {
+//         result = "Số dương > Số âm";
+//     } else if (countduong(arr) < countam(arr)) {
+//         result = "Số âm > Số dương"
+//     } else {
+//         result = "Số dương = Số âm" 
+//     }
+
+//     return result;
+// }
+
+// console.log(soSanh());
